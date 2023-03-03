ec0e6174833a4d912b0e194af6af2494e87feeae
Oksanka25 <chekoksa25@gmail.com>
2023-03-02 21:27:08 -0700

    Added more time to each question

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ function initial() {
     quizContainer.innerHTML = "";
     questionCount = 0;
     scoreCount = 0;
-    count = 11;
+    count = 25;
     clearInterval(countdown);
     timerDisplay();
     quizCreator();
