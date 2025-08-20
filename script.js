10e4f13e2375516e80014e2fe7c696ba3a024c78
Andrii <chekanovskiyacc@gmail.com>
2025-08-19 21:55:58 -0600

    Add Electrical Trivia quiz option

--- script.js
+++ script.js
@@ -18,193 +18,253 @@ let currentVolumeName;
 
 //To add new volumes
 const volumes = [
-    { label: "Start Volume 1", filePath: "volume1" },
-    { label: "Start Volume 2", filePath: "volume2" },
-    { label: "Start Volume 3", filePath: "volume3" },
-    { label: "Start Volumes 1-3", filePath: "volume1-3" },
-    { label: "Start Volume 4", filePath: "volume4" },
-    { label: "Start Volume 5", filePath: "volume5" },
-    { label: "Start Volume 6", filePath: "volume6" },
-    { label: "Start Volume 7", filePath: "volume7" },
-    { label: "Start Volumes 4-7", filePath: "volume4-7" },
-    { label: "Start Test Volume ", filePath: "test-volume" },
+  { label: "Start Volume 1", filePath: "volume1" },
+  { label: "Start Volume 2", filePath: "volume2" },
+  { label: "Start Volume 3", filePath: "volume3" },
+  { label: "Start Volumes 1-3", filePath: "volume1-3" },
+  { label: "Start Volume 4", filePath: "volume4" },
+  { label: "Start Volume 5", filePath: "volume5" },
+  { label: "Start Volume 6", filePath: "volume6" },
+  { label: "Start Volume 7", filePath: "volume7" },
+  { label: "Start Volumes 4-7", filePath: "volume4-7" },
+  { label: "Start Test Volume ", filePath: "test-volume" },
 ];
 
 //Questions and Options array
 function loadVolume(name) {
-    let xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", `${name}.json`, false);
+  xhr.open("GET", `${name}.json`, false);
 
-    xhr.send();
+  xhr.send();
 
-    xhr.onload = function () {
-        if (this.status == 200) {
-            //console.log(this.response)
-        } else {
-            console.log("Oops something went wrong");
-        }
-    };
+  xhr.onload = function () {
+    if (this.status == 200) {
+      //console.log(this.response)
+    } else {
+      alert("Oops something went wrong");
+    }
+  };
 
-    return xhr.response;
+  return xhr.response;
 }
 
-let quizArray = [];
+let questions = [];
 
 //Restart Quiz
 restartBtn.addEventListener("click", () => {
-    initial(currentVolumeName);
-    displayContainer.style.display = "block";
-    scoreContainer.classList.add("hide");
+  displayQuizByVolume(currentVolumeName);
+  displayContainer.style.display = "block";
+  scoreContainer.classList.add("hide");
 });
 
 homeBtns.forEach((button) =>
-    button.addEventListener("click", () => {
-        displayContainer.style.display = "none";
-        startScreen.style.display = "grid";
-        scoreContainer.classList.add("hide");
-    })
+  button.addEventListener("click", () => {
+    displayContainer.style.display = "none";
+    startScreen.style.display = "grid";
+    scoreContainer.classList.add("hide");
+  })
 );
 
 //Next Button
 nextBtn.addEventListener(
-    "click",
-    (displayNext = () => {
-        //increment questionCount
-        questionCount += 1;
-        //if last question
-        if (questionCount == quizArray.length) {
-            //hide question container and display score
-            displayContainer.style.display = "none";
-            scoreContainer.classList.remove("hide");
-            //user score
-            userScore.innerHTML = `Your score is ${scoreCount} out of ${questionCount} <br> <strong>${(scoreCount / questionCount).toFixed(1) * 100
-                }%</strong>`;
-        } else {
-            //display questionCount
-            countOfQuestion.innerHTML =
-                questionCount + 1 + " of " + quizArray.length + " Question";
-            //display quiz
-            quizDisplay(questionCount);
-            timerSeconds = 46;
-            clearInterval(countdown);
-            timerDisplay();
-        }
-    })
+  "click",
+  (displayNext = () => {
+    //increment questionCount
+    questionCount += 1;
+    //if last question
+    if (questionCount == questions.length) {
+      //hide question container and display score
+      displayContainer.style.display = "none";
+      scoreContainer.classList.remove("hide");
+      //user score
+      userScore.innerHTML = `Your score is ${scoreCount} out of ${questionCount} <br> <strong>${
+        (scoreCount / questionCount).toFixed(1) * 100
+      }%</strong>`;
+    } else {
+      //display questionCount
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + questions.length + " Question";
+      //display quiz
+      quizDisplay(questionCount);
+      timerSeconds = 46;
+      clearInterval(countdown);
+      timerDisplay();
+    }
+  })
 );
 
 //Timer
 const timerDisplay = () => {
-    countdown = setInterval(() => {
-        timerSeconds--;
-        timeLeft.innerHTML = `${timerSeconds}s`;
-        if (timerSeconds == 0) {
-            clearInterval(countdown);
-            displayNext();
-        }
-    }, 1000);
+  countdown = setInterval(() => {
+    timerSeconds--;
+    timeLeft.innerHTML = `${timerSeconds}s`;
+    if (timerSeconds == 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
 };
 
 //Display quiz
 const quizDisplay = (questionCount) => {
-    let quizCards = document.querySelectorAll(".container-mid");
-    //Hide other cards
-    quizCards.forEach((card) => {
-        card.classList.add("hide");
-    });
-    //display current question card
-    quizCards[questionCount].classList.remove("hide");
+  let quizCards = document.querySelectorAll(".container-mid");
+  //Hide other cards
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  //display current question card
+  quizCards[questionCount].classList.remove("hide");
 };
 
-//Quiz Creation
-function quizCreator(volumeName) {
-    //randomly sort questions
-    quizArray = JSON.parse(loadVolume(volumeName));
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
 
-    quizArray.sort(() => Math.random() - 0.5);
-    //generate quiz
-    for (let i of quizArray) {
-        //randomly sort options
-        i.options.sort(() => Math.random() - 0.5);
-        //quiz card creation
-        let div = document.createElement("div");
-        div.classList.add("container-mid", "hide");
-        //question number
-        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
-        //question
-        let question_DIV = document.createElement("p");
-        question_DIV.classList.add("question");
-        question_DIV.innerHTML = i.question;
-        div.appendChild(question_DIV);
-        //options
-        div.innerHTML += `
+  //if user clicked answer == correct option stored in object
+  if (userSolution === questions[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    //For marking the correct option
+    options.forEach((element) => {
+      if (element.innerText == questions[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  //clear interval(stop timer)
+  clearInterval(countdown);
+  //disable all options
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+function generateQuizByQuestions() {
+  //randomly sort questions
+  questions.sort(() => Math.random() - 0.5);
+  //generate quiz
+  for (let i of questions) {
+    //randomly sort options
+    i.options.sort(() => Math.random() - 0.5);
+    //quiz card creation
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    //question number
+    countOfQuestion.innerHTML = 1 + " of " + questions.length + " Question";
+    //question
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    //options
+    div.innerHTML += `
     <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
      <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
       <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
        <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
     `;
-        quizContainer.appendChild(div);
-    }
+    quizContainer.appendChild(div);
+  }
 }
 
-//Checker Function to check if option is correct or not
-function checker(userOption) {
-    let userSolution = userOption.innerText;
-    let question =
-        document.getElementsByClassName("container-mid")[questionCount];
-    let options = question.querySelectorAll(".option-div");
-
-    //if user clicked answer == correct option stored in object
-    if (userSolution === quizArray[questionCount].correct) {
-        userOption.classList.add("correct");
-        scoreCount++;
-    } else {
-        userOption.classList.add("incorrect");
-        //For marking the correct option
-        options.forEach((element) => {
-            if (element.innerText == quizArray[questionCount].correct) {
-                element.classList.add("correct");
-            }
-        });
-    }
-
-    //clear interval(stop timer)
-    clearInterval(countdown);
-    //disable all options
-    options.forEach((element) => {
-        element.disabled = true;
-    });
+function generateQuizByVolume(volumeName) {
+  questions = JSON.parse(loadVolume(volumeName));
+  generateQuizByQuestions();
 }
 
 //initial setup
-function initial(volumeName) {
-    currentVolumeName = volumeName;
-    startScreen.style.display = "none";
-    displayContainer.style.display = "block";
-    quizContainer.innerHTML = "";
-    questionCount = 0;
-    scoreCount = 0;
-    timerSeconds = 46;
+function displayQuizByVolume(volumeName) {
+  currentVolumeName = volumeName;
+  startScreen.style.display = "none";
+  displayContainer.style.display = "block";
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  timerSeconds = 46;
 
-    clearInterval(countdown);
-    timerDisplay();
-    quizCreator(volumeName);
-    quizDisplay(questionCount);
+  clearInterval(countdown);
+  timerDisplay();
+  generateQuizByVolume(volumeName);
+  quizDisplay(questionCount);
+}
+
+function displayQuizByQuestions() {
+  startScreen.style.display = "none";
+  displayContainer.style.display = "block";
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  timerSeconds = 46;
+
+  clearInterval(countdown);
+  timerDisplay();
+  generateQuizByQuestions(questions);
+  quizDisplay(questionCount);
 }
 
 function createVolumeButton(label) {
-    let button = document.createElement("button");
-    button.className = "primary-button";
-    button.innerText = label;
-    return button;
+  let button = document.createElement("button");
+  button.className = "primary-button";
+  button.innerText = label;
+  return button;
 }
 
-//when user click on start button
 volumes.forEach((volume) => {
-    const createdButton = createVolumeButton(volume.label);
-    startScreen.appendChild(createdButton);
+  const createdButton = createVolumeButton(volume.label);
+  startScreen.appendChild(createdButton);
 
-    createdButton.addEventListener("click", () => {
-        initial(volume.filePath);
-    });
+  createdButton.addEventListener("click", () => {
+    displayQuizByVolume(volume.filePath);
+  });
 });
+
+function getQuestionsByVolumes(volumes) {
+  const questions = [];
+
+  for (const volume of volumes) {
+    questions.push(...JSON.parse(loadVolume(volume)));
+  }
+
+  return questions;
+}
+
+function initializeTriviaVolume() {
+  let triviaQuestionsNumber = parseInt(
+    localStorage.getItem("trivia-questions-number"),
+    10
+  );
+
+  if (isNaN(triviaQuestionsNumber) || triviaQuestionsNumber <= 0) {
+    triviaQuestionsNumber = 25;
+    localStorage.setItem("trivia-questions-number", triviaQuestionsNumber);
+  }
+
+  const volumes = [
+    "volume1",
+    "volume2",
+    "volume3",
+    "volume4",
+    "volume4",
+    "volume5",
+    "volume6",
+    "volume7",
+  ];
+
+  questions = getQuestionsByVolumes(volumes).slice(0, triviaQuestionsNumber);
+
+  const triviaVolumeButton = createVolumeButton("Electrical Trivia");
+  startScreen.appendChild(triviaVolumeButton);
+
+  triviaVolumeButton.addEventListener("click", () => {
+    displayQuizByQuestions();
+  });
+}
+
+initializeTriviaVolume();
